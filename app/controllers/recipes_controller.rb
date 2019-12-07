2000e5b387ec81cf8c05303b62e88c74e21c9af0
henriquesml <henrique_schmeller@hotmail.com>
2019-12-07 09:55:14 -0300

    Criando Show das receitas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,11 @@
 class RecipesController < ApplicationController
 
     def index
-
         @recipes = Recipe.all
+    end
 
+    def show
+        @recipe = Recipe.find(params[:id])
     end
 
 end
