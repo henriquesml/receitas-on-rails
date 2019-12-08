94518244d17ebf28e28dff901dea153f5baf4259
henriquesml <henrique_schmeller@hotmail.com>
2019-12-08 14:39:10 -0300

    Criação de validações

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,16 @@
 class Recipe < ApplicationRecord
 
+    validates :name, :stuff, :duration, :prepare_mode, presence:true
+    validates_length_of :stuff, maximum: 200
+    validates_length_of :prepare_mode, maximum: 300
+    validates_numericality_of :calories, greather_than_or_equal_to: 0, presence:true
+    validates_format_of :poster, with: /\w+\.(jpeg|png)\z/i, message: "Utilizar somente arquivos JPEG ou PNG."
+    
+    KIND = %w(Carner Aves Peixes Massas Saladas Doces)
+    validates_inclusion_of :kind, in: KIND
+
     def light?
-        calories < 100
+        calories.blank? || calories < 100
     end
 
 end
